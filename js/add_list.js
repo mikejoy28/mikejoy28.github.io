5c5f05908ba0770787ac93b4dd5ba60be3b2a1c4
miketommy <goodm00i00@gmail.com>
2020-01-11 12:14:04 +0800

    Add files via upload

---
+++ js/add_list.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    function change_margin_top(w, h){
+        if(w < 1100)
+        {
+            console.log("enter");
+            $("#demo_content").css("margin-top", 0 + "px");
+        }
+        else
+        {
+            $("#demo_content").css("margin-top", -h + "px");
+        }
+    }
+    var current_window = 0;
+    var add_list_var = $("#home").outerHeight();
+    var check = false;
+    /*get windows size*/
+    var w = $(window).width();
+    var h = $(window).height();
+    change_margin_top(w, h);
+    $("#demo_list").css("height", h + "px");
+
+    /*windows reszie*/ 
+    $(window).resize(function() {
+        w = $(window).width();
+        h = $(window).height();
+        change_margin_top(w,h);
+        
+    });
+    
+     
+    $(window).scroll(function(){
+        
+        current_window = $(this).scrollTop();
+        
+        
+        if(check == false && current_window >= h -  $("#get_hight").height())
+        {
+            check = true;
+            var margin_top = $("#get_hight").height() - h;
+            $("#demo_list").css("position", "fixed");
+            $("#demo_list").css("margin-top", margin_top + "px");
+            $("#demo_list").css("height", "100%");
+            $("#demo_content").css("margin-top", "0px");
+        }
+        else if(check == true && current_window < h-  $("#get_hight").height())
+        {
+            $("#demo_list").css("position", "relative");
+            $("#demo_list").css("margin-top", "0");
+            $("#demo_list").css("height", h + "px");
+            change_margin_top(w, h);
+            check = false;
+        }
+    });   
+});
---
+++ upload/js/upload.js
@@ -0,0 +1,65 @@
+
+$(function () {
+	var upload_file_name;
+	var upload_file_size;
+	var upload_file_type;
+	var $upload_drop = $(".dropbox");
+	
+	function stopEvent(evt) {
+		evt.stopPropagation();
+		evt.preventDefault();
+	}
+	$upload_drop.bind("dragover", function (e) {
+		//滑鼠經過上方時加入特效
+		stopEvent(e);
+		$(e.target).addClass("hover");
+	}).bind("dragleave", function (e) {
+		//滑鼠移開時移除特效
+		stopEvent(e);
+		$(e.target).removeClass("hover");
+	}).bind("drop", function (e) {
+		//拖放操作完成事件
+		stopEvent(e);
+		$(e.target).removeClass("hover");
+		//由dataTransfer.files取得檔案資訊
+		var files = e.originalEvent.dataTransfer.files;
+		var imageFiles = $.map(files, function (f, i) {
+			upload_file_type = f.type;
+			return f;
+		});
+		if ($("#h2_content") != null) {
+			$("#h2_content").remove();
+		}
+		var files = e.originalEvent.dataTransfer.files;
+		var imageFiles = $.map(files, function (f, i) {
+			return f;
+		});
+		$.each(imageFiles, function (i, file) {
+			upload_file_name = file.name;
+			upload_file_size = file.size;
+		});
+
+
+		var div_content = '<div class="border m-2 d-inline-block p-4"><p class="pt-0 block_font_size">' + 
+							upload_file_name +' ' + (upload_file_size / 1024).toFixed(2) + 'KB' 
+							+ '</p> <p class="pt-0 block_font_size">' + upload_file_type +
+		  					' </p> <div "class="progress" id = "progress_content"><div class = "aa">\
+		  					<div class="progress-bar progress-bar-striped  \
+		  					change_value background_animation" style="width : 100%">0%</div></div></div></div>';
+		$(".dropbox").append(div_content);
+
+		var element = document.getElementsByClassName("change_value");
+		var time = 0;
+		var id = window.setInterval(time_going, 6);
+		function time_going() {
+			time = time + 1;
+			element[element.length - 1].innerHTML = time + "%";
+			if (time == 100) {
+				clearInterval(id);
+				time = 0;
+			}
+
+		}
+
+	});
+});
